6524be9e2a330ef512bf6d56b3f2f7d934ee5ce0
aidilbaihaqi <aidillmusirjun@gmail.com>
2024-11-30 13:52:38 +0700

    setting layout: main, components: topbar,navbar,footer

--- resources/views/components/topbar.blade.php
+++ resources/views/components/topbar.blade.php
@@ -2,25 +2,29 @@
   <div class="row align-items-center bg-light px-lg-5">
       <div class="col-12 col-md-8">
           <div class="d-flex justify-content-between">
-              <div class="bg-primary text-white text-center py-2" style="width: 100px;">Tranding</div>
+              <div class="bg-primary text-white text-center py-2" style="width: 100px;">Global news</div>
               <div class="owl-carousel owl-carousel-1 tranding-carousel position-relative d-inline-flex align-items-center ml-3" style="width: calc(100% - 100px); padding-left: 90px;">
-                  <div class="text-truncate"><a class="text-secondary" href="">Labore sit justo amet eos sed, et sanctus dolor diam eos</a></div>
-                  <div class="text-truncate"><a class="text-secondary" href="">Gubergren elitr amet eirmod et lorem diam elitr, ut est erat Gubergren elitr amet eirmod et lorem diam elitr, ut est erat</a></div>
+                  <div class="text-truncate"><a class="text-secondary" href="">Berita Satu: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus enim beatae vel quod expedita reprehenderit earum aspernatur dolorum dolore rem.</a></div>
+                  <div class="text-truncate"><a class="text-secondary" href="">Berita Dua: Lorem, ipsum dolor sit amet consectetur adipisicing elit. A laboriosam esse tempora corporis optio voluptatum debitis assumenda neque repellendus laborum?</a></div>
               </div>
           </div>
       </div>
       <div class="col-md-4 text-right d-none d-md-block">
-          Monday, January 01, 2045
+        @php
+            use Carbon\Carbon;
+            Carbon::setLocale('id');
+        @endphp
+        {{ Carbon::now()->translatedFormat('l, j F Y') }}
       </div>
   </div>
   <div class="row align-items-center py-2 px-lg-5">
       <div class="col-lg-4">
-          <a href="" class="navbar-brand d-none d-lg-block">
-              <h1 class="m-0 display-5 text-uppercase"><span class="text-primary">News</span>Room</h1>
+          <a href="{{ route('front.index') }}" class="navbar-brand d-none d-lg-block">
+              <h1 class="m-0 display-5 text-uppercase"><span class="text-primary">Risalah </span>Maritim</h1>
           </a>
       </div>
       <div class="col-lg-8 text-center text-lg-right">
-          <img class="img-fluid" src="img/ads-700x70.jpg" alt="">
+          <img class="img-fluid" src="{{ asset('img/ads-700x70.jpg') }}" alt="bannerads">
       </div>
   </div>
 </div>
