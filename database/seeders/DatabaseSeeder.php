0b99b4fbbfd9e13ad7bb24ec815ee6bdecfe6bee
aidilbaihaqi <aidillmusirjun@gmail.com>
2024-11-30 21:40:55 +0700

    add categoryseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -33,7 +33,8 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $this->call([
-            MemberSeeder::class
+            MemberSeeder::class,
+            CategorySeeder::class
         ]); 
     }
 }
